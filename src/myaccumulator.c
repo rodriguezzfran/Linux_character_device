1e9dabd201797f17604f71b647f81e92461e577e
Rodriguezzfran <francorod47@gmail.com>
2025-04-11 19:33:43 -0300

    added doxygen documentation style

--- src/myaccumulator.c
+++ src/myaccumulator.c
@@ -19,20 +19,29 @@ static struct class *accumClass = NULL; // Device class structure, used to creat
 
 static long long accumulator = 0; // Accumulator variable to store the sum
 
+/**
+ * @brief Handles attempts to open the character device.
+ *
+ * Only allows access if the calling process has root privileges.
+ * Logs access using pr_info and pr_warn.
+ *
+ * @param inode Pointer to the inode structure associated with the device.
+ * @param file Pointer to the file structure representing the open file.
+ * 
+ * @return 0 on success, or -EACCES if access is denied (not root).
+ */
 static int device_open(struct inode *inode, struct file *file) {
 
     kuid_t uid = current_uid();
     pid_t pid = current->pid;
     const char *cmd = current->comm;
 
-    /*
     // Only allow access if the UID is root
     if (uid.val != 0) {
         pr_warn("myaccumulator: Device open denied - PID %d, UID %d, comando: %s\n",
                 pid, uid.val, cmd);
         return -EACCES;  
     }
-    */
 
     // If everything is ok, we can open the device
     pr_info("myaccumulator: Device opened - PID %d, UID %d, comando: %s\n",
@@ -41,6 +50,16 @@ static int device_open(struct inode *inode, struct file *file) {
 
 }
 
+/**
+ * @brief Handles the release operation on the device.
+ *
+ * Logs the release event with the PID of the calling process.
+ *
+ * @param inode Pointer to the inode structure associated with the device.
+ * @param file Pointer to the file structure representing the open file.
+ * 
+ * @return Always returns 0.
+ */
 static int dev_release(struct inode *inode, struct file *file) {
     
     pr_info("myaccumulator: Device closed by PID %d\n", current->pid);
@@ -48,10 +67,28 @@ static int dev_release(struct inode *inode, struct file *file) {
     return 0;
 }
 
+/**
+ * @brief Read the current value of the accumulator from the device.
+ * 
+ * This function copies the current value of the accumulator to user space
+ * when the device is read. It returns the value only once per open-read cycle
+ *
+ * @param file Pointer to the file object (unused but required by the function signature).
+ * @param buffer User space buffer where the data will be copied.
+ * @param len Number of bytes requested to read.
+ * @param offset Offset in the file (used to return 0 on second read).
+ * 
+ * @return ssize_t Number of bytes read on success, 0 if already read (EOF), 
+ *         or a negative error code on failure.
+ */
 static ssize_t dev_read(struct file *file, char __user *buffer, size_t len, loff_t *offset) {
     
     char *user_data; // Buffer to store the data to be read
     int size;
+
+    if (*offset > 0) {
+        return 0; // Simulate EOF on subsequent reads
+    }
 
     user_data = kmalloc(BUFFER_SIZE, GFP_KERNEL);
     if (!user_data) {
@@ -59,34 +96,48 @@ static ssize_t dev_read(struct file *file, char __user *buffer, size_t len, loff
         return -ENOMEM;
     }
 
-    if (*offset  > 0) {
-        return 0; // avoid reading again, a new read will see the end of the file
-    }
-
     // snprintf is used to format the string, it returns the number of bytes written
     size = snprintf(user_data, BUFFER_SIZE, "%lld\n", accumulator);
 
     // Check if the size is greater than the buffer size
     if (size < 0) {
+        kfree(user_data); // Free the kernel buffer
         pr_err("myaccumulator: Error formatting the string\n");
         return -EFAULT;
     }
 
     // Copy the data to user space
     if (copy_to_user(buffer, user_data, size)) {
+        kfree(user_data); // Free the kernel buffer
         pr_err("myaccumulator: Error copying data to user space\n");
         return -EFAULT;
     }
 
     // Update the offset
     *offset += size;
+    kfree(user_data); // Free the kernel buffer
 
     // put a message in the kernel log
     pr_info("myaccumulator: Read %lld from device\n", accumulator);
-
     return size; // Return the number of bytes read
 }
 
+/**
+ * @brief Handles write operations to the device.
+ *
+ * Receives a string from the user space representing an integer value,
+ * converts it to a long, and adds it to the internal accumulator.
+ *
+ * @param file   Pointer to the file structure.
+ * @param buffer Pointer to the user-space buffer containing the data.
+ * @param len    Length of the data to write.
+ * @param offset Pointer to the file offset (not used but required for the function signature).
+ *  
+ * @return Number of bytes written on success, or a negative error code on failure:
+ *         - -ENOMEM if memory allocation fails.
+ *         - -EFAULT if copy_from_user fails.
+ *         - -EINVAL if string conversion fails.
+ */
 static ssize_t dev_write (struct file *file, const char __user *buffer, size_t len, loff_t *offset) {
     
     char *kbuf; // Kernel buffer to store the data from user space
@@ -132,13 +183,20 @@ static struct file_operations fops = {
     .write = dev_write,
 };
 
-// Module initialization function
+/**
+ * @brief Initialize the myaccumulator kernel module.
+ *
+ * This function allocates a character device region, initializes the character device,
+ * creates the associated class and device in /dev/ and register some messages in the kernel log.
+ *
+ * @return 0 on success, negative error code on failure.
+ */
 static int __init myaccumulator_init(void) {
 
     // Allocate a device number, 0 means it is the first device, 1 means the amount of devices.
     if( alloc_chrdev_region(&dev_num, 0, 1, DEVICE_NAME) < 0) {
         pr_err("myaccumulator: Failed to allocate device number\n");
-        return -1; // Return error if allocation fails
+        return -EBUSY; // Return error if allocation fails
     }else{
         pr_info("myaccumulator: Registered with major number: %d\n", MAJOR(dev_num));
     }
@@ -151,7 +209,7 @@ static int __init myaccumulator_init(void) {
     if (cdev_add(&accumDevice, dev_num, 1) < 0) {
         pr_err("myaccumulator: Failed to add device to system\n");
         unregister_chrdev_region(dev_num, 1); // Unregister the device number
-        return -1; // Return error if adding fails
+        return -EINVAL; // Return error if adding fails
     }else{
         pr_info("myaccumulator: Device added to system\n");
     }
@@ -173,7 +231,7 @@ static int __init myaccumulator_init(void) {
         class_destroy(accumClass); // Destroy the class
         cdev_del(&accumDevice); // Delete the device
         unregister_chrdev_region(dev_num, 1); // Unregister the device number
-        return -1; // Return error if device creation fails
+        return -ENODEV; // Return error if device creation fails
     }else{
         pr_info("myaccumulator: Device created\n");
     }
@@ -181,7 +239,13 @@ static int __init myaccumulator_init(void) {
     return 0; 
 }
 
-// Module exit function
+/**
+ * @brief Module cleanup function.
+ *
+ * This function is called when the module is removed from the kernel.
+ * It performs cleanup by destroying the device, class, character device,
+ * and unregistering the allocated device number.
+ */
 static void __exit myaccumulator_exit(void) {
     
     device_destroy(accumClass, dev_num); // Destroy the device
